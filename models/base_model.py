df79e3d7f42c245a4539c549aac22676bb563f87
Ganzadidier <d.ganza@alustudent.com>
2024-09-11 05:39:15 -0700

    indetation

--- models/base_model.py
+++ models/base_model.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-      print(
+        print(
           "\t{}: ({}) - {}".format(
               key,
               type(my_model_json[key]),
